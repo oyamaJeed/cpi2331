62fd7a9920c1814290dd92d5ca460418fe4c8046
meit2331 <meit2331@kinki-pc.ac.jp>
2023-05-08 02:56:43 -0700

    [Add]ptr_exer1.c.

--- ptr_exer1.c
+++ ptr_exer1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ARY_MAX 3
-#define STR_MAX 10
-char **get_modify_str_ary(int len_ary1,char inp_ary1);
+
+char **get_modify_str_ary(char inp_ary1[][8],int ary1_line);
 
 int main(int argc ,char *argv[])
 {
@@ -11,7 +12,7 @@ int main(int argc ,char *argv[])
 		printf("Before str ary[%d]=%s\n",i,inp_ary1[i]);
 	}
 	
-	get_modify_str_ary(3,inp_ary1);
+	**get_modify_str_ary(inp_ary1,3);
 	
 	printf("------------------------------------\n");
 	for(int k=0; k<ARY_MAX;k++){
@@ -21,17 +22,13 @@ int main(int argc ,char *argv[])
 	return 0;
 }
 
-char **get_modify_str_ary(int len_ary1,char inp_ary1[][8]){
+char **get_modify_str_ary(char inp_ary1[][8],int ary1_line){
 	char add_str[][2] = {"e","e","a"};
-	char new_ary[][8];
-	for(int i=0;i<ARY_MAX;i++){
-		for(int t=0;t<STR_MAX;t++){
-			new_ary[i][t] = inp_ary1[i][t];
-			if(inp_ary1[i][t]=='\0'){
-				new_ary[i][t] = add_str[i][2];
-			}
-		}
+	for(int i=0;i<ary1_line;i++){
+		strcat(inp_ary1[i],add_str[i]);
+	//	printf("%s",inp_ary1[i]);
 	}
-	return new_ary;
+		
+		
 }
 
